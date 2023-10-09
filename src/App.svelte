31bf2fe9730f11380d2708083b141f66fc2c1d83
Joé Veber <cc42dev@gmail.com>
2023-10-09 18:45:58 +0200

    workflow

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,7 @@
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
-  <h1>LIFE IS PERFECT DD d</h1>
+  <h1>LIFE IS MAGNIFICANT</h1>
 
   <div class="card">
     <Counter />
